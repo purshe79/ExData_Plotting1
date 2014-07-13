0fdc5c181a82c374305584cca576027f959657f7
purshe79 <puru.polaris@gmail.com>
2014-07-13 19:28:00 -0400

    submission of first plot

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data <-read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=FALSE, sep=';')
+colnames(data) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+hist(data$Global_active_power, xlab="Global Active Power(kilowatts)", main="Global Active Power",col="Red")
+dev.copy(png,file="plot1.png")
+dev.off()
